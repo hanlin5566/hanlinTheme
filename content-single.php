20888c66e39dac08f1b60af10e160d327c84f9af
hanlin.hu <hanlin5566@foxmail.com>
2017-02-09 15:52:23 +0800

    添加版权说明与百度分享
    添加版权说明与百度分享

--- content-single.php
+++ content-single.php
@@ -11,6 +11,11 @@
   <div class="entry clearfix">
     <?php
      the_content();
+	?>
+	<div class="postcopyright">
+		<strong>©版权声明：</strong>本文为【<a href="http://www.huhanlin.com" >翰林小院</a>】（<a href="http://www.huhanlin.com" >huhanlin.com</a>）原创文章，转载时请注明出处！
+	</div>
+	<?php
 		  wp_link_pages( array(
 				'before' => '<p class="page-links">' . __( 'Pages: ', 'huhanlin' ),
 				'after'  => '</p>',
@@ -40,4 +45,6 @@
 		$editpost =  sprintf( __('Edit This Post' , 'huhanlin') );
 		edit_post_link($editpost, '<p class="button editlink">', '</p>');
 	?>
+      <div class="bdsharebuttonbox"><a href="#" class="bds_more" data-cmd="more"></a><a href="#" class="bds_qzone" data-cmd="qzone" title="分享到QQ空间"></a><a href="#" class="bds_tsina" data-cmd="tsina" title="分享到新浪微博"></a><a href="#" class="bds_tqq" data-cmd="tqq" title="分享到腾讯微博"></a><a href="#" class="bds_renren" data-cmd="renren" title="分享到人人网"></a><a href="#" class="bds_weixin" data-cmd="weixin" title="分享到微信"></a></div>
+      <script>window._bd_share_config={"common":{"bdSnsKey":{},"bdText":"","bdMini":"2","bdPic":"","bdStyle":"0","bdSize":"16"},"share":{},"image":{"viewList":["qzone","tsina","tqq","renren","weixin"],"viewText":"分享到：","viewSize":"16"}};with(document)0[(getElementsByTagName('head')[0]||body).appendChild(createElement('script')).src='http://bdimg.share.baidu.com/static/api/js/share.js?v=89860593.js?cdnversion='+~(-new Date()/36e5)];</script>
 </article>
